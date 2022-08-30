1d73b5e2b5ba7b75b8e6e13745ecf093dd01165e
Fulya Sariyar <fulyasariyar@outlook.com>
2022-08-30 12:39:16 +0300

    TweetBox Design

--- twitter-clone/src/layout/Content.js
+++ twitter-clone/src/layout/Content.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import { PopulerIcon} from "../icons/Icon"
+import TweetBox from '../components/TweetBox'
+import Divider from '../components/Divider'
 
 
 const Content = () => {
   return (
-    <main className="flex-1 bg-purple-200">
-      <h1>Content</h1>
-    </main>
+    <main className="flex-1 flex flex-col border-r border-l border-gray-extraLight">
+    <header className=' sticky top-0 z-10 flex justify-between items-center p-4 border-b border-gray-extraLight bg-white'>
+      <span className='font-bold text-xl text-gray-900'>Home</span>
+      <PopulerIcon className="w-6 h-6 text-primary-base"></PopulerIcon>
+    </header>
+    <div className='flex space-x-4 px-4 py-3 '>
+    <img
+      src="https://pbs.twimg.com/profile_images/1563276255000027141/mGEHmNL-_400x400.jpg"
+      alt="Profile"
+      className="w-11 h-11 rounded-full"
+    />
+     <TweetBox></TweetBox>
+    </div>
+    <Divider></Divider>
+  </main>
   )
 }
 
--- twitter-clone/src/layout/Widgets.js
+++ twitter-clone/src/layout/Widgets.js
@@ -1,29 +1,31 @@
-import React from 'react'
-import {SearchIcon} from "../icons/Icon"
-import {Timeline} from "react-twitter-widgets"
+import React from "react";
+import { Timeline } from "react-twitter-widgets";
+import { SearchIcon } from "../icons/Icon";
 
 const Widgets = () => {
   return (
-    <aside className=" w-80">
-     <div className='flex items-center space-x-4 p-3 m-3 bg-gray-200 rounded-full text-gray-dark focus-within:bg-white focus-within:ring-1 focus-within:ring-primary-base focus-within:text-primary-base'>
-      <SearchIcon className=" w-5 h-5 "></SearchIcon>
-      <input type ="text" placeholder='Search Twitter' className=' placeholder-gray-dark bg-transparent  focus:outline-none  w-ful'></input>
-     </div>
-     <div className='mt-5 h-32 scrollbar scrollbar-thumb-gray-900 scrollbar-track-gray-100'>
-     <Timeline 
+    <aside className="w-80">
+      <div className="flex items-center space-x-4 p-3 m-3 bg-gray-200 rounded-full text-gray-dark focus-within:bg-white focus-within:ring-1 focus-within:ring-primary-base focus-within:text-primary-base">
+        <SearchIcon className="w-5 h-5" />
+        <input
+          type="text"
+          placeholder="Search Twitter"
+          className="placeholder-gray-dark bg-transparent focus:outline-none w-full text-sm"
+        />
+      </div>
+      <div className="mt-5">
+        <Timeline
           dataSource={{
             sourceType: "profile",
-            screenName: "FulyaSariyar",
+            screenName: "react",
           }}
           options={{
             height: "1000",
           }}
         />
-     
-
-     </div>
+      </div>
     </aside>
-  )
-}
+  );
+};
 
-export default Widgets
+export default Widgets;
