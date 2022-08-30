76820c53edc6ef6af9432e63c4a145506784aab6
Fulya Sariyar <fulyasariyar@outlook.com>
2022-08-30 11:24:10 +0300

    Timeline

--- twitter-clone/src/layout/Widgets.js
+++ twitter-clone/src/layout/Widgets.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import {SearchIcon} from "../icons/Icon"
+import {Timeline} from "react-twitter-widgets"
 
 const Widgets = () => {
   return (
-    <div className="bg-green-200 w-80">
-      <h1>Timeline</h1>
-    </div>
+    <aside className=" w-80">
+     <div className='flex items-center space-x-4 p-3 m-3 bg-gray-200 rounded-full text-gray-dark focus-within:bg-white focus-within:ring-1 focus-within:ring-primary-base focus-within:text-primary-base'>
+      <SearchIcon className=" w-5 h-5 "></SearchIcon>
+      <input type ="text" placeholder='Search Twitter' className=' placeholder-gray-dark bg-transparent  focus:outline-none  w-ful'></input>
+     </div>
+     <div className='mt-5 h-32 scrollbar scrollbar-thumb-gray-900 scrollbar-track-gray-100'>
+     <Timeline 
+          dataSource={{
+            sourceType: "profile",
+            screenName: "FulyaSariyar",
+          }}
+          options={{
+            height: "1000",
+          }}
+        />
+     
+
+     </div>
+    </aside>
   )
 }
 
